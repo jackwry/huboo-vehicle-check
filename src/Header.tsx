504f05b8e40ef864eb2513feaac6fa4cd0b97e42
Jack Ryan <bandista.dev@protonmail.com>
2022-02-02 18:27:54 +0000

    web project moved to web folder

--- src/Header.tsx
+++
@@ -1,9 +0,0 @@
-const Header: React.FC = () => (
-  <div className="w-screen py-8 mb-4 border-b-2 border-gray">
-    <div className="container mx-auto px-4 object-left text-2xl">
-      Huboo Vehicle Checker
-    </div>
-  </div>
-);
-
-export default Header;
---
+++ web/src/Header.tsx
@@ -0,0 +1,9 @@
+const Header: React.FC = () => (
+  <div className="w-screen py-8 mb-4 border-b-2 border-gray">
+    <div className="container mx-auto px-4 object-left text-2xl">
+      Huboo Vehicle Checker
+    </div>
+  </div>
+);
+
+export default Header;
